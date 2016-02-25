5283a0e1e08d9675ccf729a9a54ace062c008fca
Anton Khlynovskiy <subzey@gmail.com>
2016-02-25 15:16:23 +0300

    WIP

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
-var regexpClassString = require('./lib/regexpclass').classString;
+var serializeRanges = require('./lib/regexpclass').serializeRanges;
 
-console.log(regexpClassString([
-	{from: 0x5E, to: 0x60},
-	{from: 0x2D, to: 0x30},
+// console.log(serializeRanges([
+// 	{from: 0x5E, to: 0x60},
+// 	{from: 0x2D, to: 0x30},
+// ]));
+
+console.log(serializeRanges([
+	{from: 0x21, to: 0x22},
+]));
+
+console.log(serializeRanges([
+	{from: 0x21, to: 0x23},
 ]));
